e0de70294a9eb726f62444410da8272dac9a2692
admirallightningbolt <aviknecht@gmail.com>
2019-01-07 19:18:45 -0800

    Removed unused import.

--- src/main/java/spyr/cards/red/Revenge.java
+++ src/main/java/spyr/cards/red/Revenge.java
@@ -6,9 +6,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import spyr.cards.SpyrCard;
