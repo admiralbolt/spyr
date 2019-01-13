50f8abc2be8ed844756293f8d03988ec85d34a4e
admiralbolt <aviknecht@gmail.com>
2019-01-12 17:19:40 -0800

    Updated copy_latest script to work on linux or mac.

--- lib/copy_latest.sh
+++ lib/copy_latest.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-cp ~/Library/Application\ Support/Steam/steamapps/common/SlayTheSpire/SlayTheSpire.app/Contents/Resources/desktop-1.0.jar .
-cp -r ~/Library/Application\ Support/Steam/steamapps/common/SlayTheSpire/SlayTheSpire.app/Contents/Resources/preferences .
+MAC_BASEPATH=~/Library/Application\ Support/Steam/steamapps/common/SlayTheSpire/SlayTheSpire.app/Contents/Resources/
+LINUX_BASEPATH=~/.steam/steam/steamapps/common/SlayTheSpire/
+if [ -d "${MAC_BASEPATH}" ];
+then
+  BASEPATH=$MAC_BASEPATH
+else
+  BASEPATH=$LINUX_BASEPATH
+fi
 
+cp "${BASEPATH}/desktop-1.0.jar" .
+cp -r "${BASEPATH}/preferences" .
