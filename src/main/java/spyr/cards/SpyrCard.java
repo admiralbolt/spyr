7c424d024055596ea0e43261187c8ff45dfec53c
admiralbolt <aviknecht@gmail.com>
2019-01-13 13:28:19 -0800

    More cleanup of dual card descriptions. Streamlined helper functions and fixed upgraded card descriptions as well as added suffix support.

--- src/main/java/spyr/cards/SpyrCard.java
+++ src/main/java/spyr/cards/SpyrCard.java
@@ -26,18 +26,15 @@ public abstract class SpyrCard extends CustomCard {
 
 	public CardStrings cardStrings;
 
-	public SpyrCard(String id, int cost, CardType type, CardColor color,
-			CardRarity rarity, CardTarget target) {
-		this(id, CardCrawlGame.languagePack.getCardStrings(id), cost, type, color,
-				rarity, target);
+	public SpyrCard(String id, int cost, CardType type, CardColor color, CardRarity rarity, CardTarget target) {
+		this(id, CardCrawlGame.languagePack.getCardStrings(id), cost, type, color, rarity, target);
 	}
 
-	private SpyrCard(String id, CardStrings cardStrings, int cost, CardType type,
-			CardColor color, CardRarity rarity, CardTarget target) {
+	private SpyrCard(String id, CardStrings cardStrings, int cost, CardType type, CardColor color, CardRarity rarity,
+			CardTarget target) {
 		super(id, cardStrings.NAME,
-				String.format("spyr/images/cards/%s/%s/%s.png",
-						Spyr.COLOR_MAP.get(color), type.toString().toLowerCase(),
-						id.split(":")[1]),
+				String.format("spyr/images/cards/%s/%s/%s.png", Spyr.COLOR_MAP.get(color),
+						type.toString().toLowerCase(), id.split(":")[1]),
 				cost, cardStrings.DESCRIPTION, type, color, rarity, target);
 		this.cardStrings = cardStrings;
 	}
@@ -63,8 +60,7 @@ public abstract class SpyrCard extends CustomCard {
 	public AbstractCard makeCopy() {
 		try {
 			return this.getClass().getConstructor().newInstance();
-		} catch (InstantiationException | IllegalAccessException
-				| IllegalArgumentException | InvocationTargetException
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
 				| NoSuchMethodException | SecurityException e) {
 			e.printStackTrace();
 			return null;
@@ -86,51 +82,53 @@ public abstract class SpyrCard extends CustomCard {
 	}
 
 	/**
+	 * This is a helper function used to correctly generate dual card descriptions
+	 * outside of combat. Card descriptions for dual cards are loaded in the
+	 * following way from the strings in spyr_cards.json:
+	 *
+	 * DESCRIPTION -> Is a suffix that is always added if non-empty.
+	 * EXTENDED_DESCRIPTION[0] -> Non-upgraded shadow form text.
+	 * EXTENDED_DESCRIPTION[1] -> Non-upgraded light form text.
+	 * EXTENDED_DESCRIPTION[2] -> Upgraded shadow form text (if non-empty).
+	 * EXTENDED_DESCRIPTION[3] -> Upgraded light form text (if non-empty).
+	 *
+	 * New lines are added in between the shadow form text, light form text, and
+	 * suffix text.
+	 */
+	public String getDualCardDescription(boolean hasShadow, boolean hasLight) {
+		StringBuilder description = new StringBuilder();
+		int shadowIndex = (this.upgraded && this.cardStrings.EXTENDED_DESCRIPTION[2] != null
+				&& !this.cardStrings.EXTENDED_DESCRIPTION[2].isEmpty()) ? 2 : 0;
+		int lightIndex = (this.upgraded && this.cardStrings.EXTENDED_DESCRIPTION[3] != null
+				&& !this.cardStrings.EXTENDED_DESCRIPTION[3].isEmpty()) ? 3 : 1;
+		if (hasShadow) {
+			description.append("ShadowForm: ");
+			description.append(this.cardStrings.EXTENDED_DESCRIPTION[shadowIndex]);
+			description.append(" NL ");
+		}
+		if (hasLight) {
+			description.append("LightForm: ");
+			description.append(this.cardStrings.EXTENDED_DESCRIPTION[lightIndex]);
+			description.append(" NL ");
+		}
+		if (this.cardStrings.DESCRIPTION != null && !this.cardStrings.DESCRIPTION.isEmpty()) {
+			description.append(this.cardStrings.DESCRIPTION);
+		}
+		return description.toString();
+	}
+
+	public void initializeDualCardDescription() {
+		this.rawDescription = this.getDualCardDescription(true, true);
+		this.initializeDescription();
+	}
+
+	/**
 	 * This is a helper function to be used to recalculate dual-card description
 	 * text during combat.
 	 */
 	public void loadDualCardDescription() {
-		StringBuilder description = new StringBuilder();
-		if (AbstractDungeon.player.hasPower(DarkEcoPower.POWER_ID)) {
-			description.append(this.cardStrings.EXTENDED_DESCRIPTION[0]);
-		}
-		if (AbstractDungeon.player.hasPower(LightEcoPower.POWER_ID)) {
-			description.append(this.cardStrings.EXTENDED_DESCRIPTION[1]);
-		}
-		this.rawDescription = description.toString();
-		this.initializeDescription();
-	}
-
-	/**
-	 * This is a helper function to be used to calculate dual-card description
-	 * outside of combat. It includes the description of both card effects.
-	 */
-	public void initializeDualCardDescription() {
-		StringBuilder description = new StringBuilder();
-		description.append("ShadowForm: ");
-		description.append(this.cardStrings.EXTENDED_DESCRIPTION[0]);
-		description.append("LightForm: ");
-		description.append(this.cardStrings.EXTENDED_DESCRIPTION[1]);
-		if (this.cardStrings.DESCRIPTION != null && !this.cardStrings.DESCRIPTION.isEmpty()) {
-			description.append(this.cardStrings.DESCRIPTION);
-		}
-		this.rawDescription = description.toString();
-		this.initializeDescription();
-	}
-
-	/**
-   * A helper function that upgrades dual-card descriptions.
-	 */
-	public void upgradeDualCardDescription() {
-		StringBuilder description = new StringBuilder();
-		description.append("ShadowForm: ");
-		description.append(this.cardStrings.EXTENDED_DESCRIPTION[2]);
-		description.append("LightForm: ");
-		description.append(this.cardStrings.EXTENDED_DESCRIPTION[3]);
-		if (this.cardStrings.DESCRIPTION != null && !this.cardStrings.DESCRIPTION.isEmpty()) {
-			description.append(this.cardStrings.DESCRIPTION);
-		}
-		this.rawDescription = description.toString();
+		this.rawDescription = this.getDualCardDescription(AbstractDungeon.player.hasPower(DarkEcoPower.POWER_ID),
+				AbstractDungeon.player.hasPower(LightEcoPower.POWER_ID));
 		this.initializeDescription();
 	}
 
