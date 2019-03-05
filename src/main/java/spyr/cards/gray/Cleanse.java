31473a8094d84c2f87bd9161e6db2e78b69440ec
admirallightningbolt <aviknecht@gmail.com>
2019-03-04 19:32:49 -0800

    updates?

--- src/main/java/spyr/cards/gray/Cleanse.java
+++ src/main/java/spyr/cards/gray/Cleanse.java
@@ -15,21 +15,23 @@ import spyr.patches.CardEnum;
 public class Cleanse extends SpyrCard {
 
 	public static final String ID = "spyr:cleanse";
-  public static final String NAME = "Cleanse";
-  public static final String DESCRIPTION = "Remove all powers from an enemey. NL Exhaust.";
+	public static final String NAME = "Cleanse";
+	public static final String DESCRIPTION = "Remove all powers from an enemey. NL Exhaust.";
 
 	private static final int COST = 2;
 	private static final int UPGRADED_COST = 1;
 
 	public Cleanse() {
-		super(ID, NAME, DESCRIPTION, COST, AbstractCard.CardType.SKILL, CardEnum.FRACTURED_GRAY, AbstractCard.CardRarity.UNCOMMON,
+		super(ID, NAME, DESCRIPTION, COST, AbstractCard.CardType.SKILL,
+				CardEnum.FRACTURED_GRAY, AbstractCard.CardRarity.UNCOMMON,
 				AbstractCard.CardTarget.ENEMY);
 		this.exhaust = true;
 	}
 
-  @Override
+	@Override
 	public void use(AbstractPlayer p, AbstractMonster m) {
-		AbstractDungeon.actionManager.addToBottom(new RemoveAllPowersAction(m, /* debuffsOnly= */false));
+		AbstractDungeon.actionManager
+				.addToBottom(new RemoveAllPowersAction(m, /* debuffsOnly= */false));
 	}
 
 	@Override
--- src/main/java/spyr/cards/gray/Contradict.java
+++ src/main/java/spyr/cards/gray/Contradict.java
@@ -17,32 +17,33 @@ import spyr.powers.TemporaryLightEcoPower;
 public class Contradict extends SpyrCard {
 
 	public static final String ID = "spyr:contradict";
-  public static final String NAME = "Contradict";
-  public static final String DESCRIPTION = "This turn, your next card is affected by both LightForm AND ShadowForm.";
-  public static final String UPGRADE_DESCRIPTION = "This turn, your next !M! cards are affected by both LightForm AND ShadowForm.";
+	public static final String NAME = "Contradict";
+	public static final String DESCRIPTION = "This turn, your next card is affected by both LightForm AND ShadowForm.";
+	public static final String UPGRADE_DESCRIPTION = "This turn, your next !M! cards are affected by both LightForm AND ShadowForm.";
 
 	private static final int COST = 1;
 	private static final int DOUBLE_AMOUNT = 1;
 	private static final int UPGRADE_DOUBLE_AMOUNT = 1;
 
 	public Contradict() {
-		super(ID, NAME, DESCRIPTION, COST, AbstractCard.CardType.SKILL, CardEnum.FRACTURED_GRAY, AbstractCard.CardRarity.RARE,
+		super(ID, NAME, DESCRIPTION, COST, AbstractCard.CardType.SKILL,
+				CardEnum.FRACTURED_GRAY, AbstractCard.CardRarity.RARE,
 				AbstractCard.CardTarget.SELF);
 		this.magicNumber = this.baseMagicNumber = DOUBLE_AMOUNT;
 	}
 
-  @Override
+	@Override
 	public void use(AbstractPlayer p, AbstractMonster m) {
-		AbstractDungeon.actionManager.addToBottom(
-				new ApplyPowerAction(p, p, new TemporaryDarkEcoPower(p, this.magicNumber), this.magicNumber));
-		AbstractDungeon.actionManager.addToBottom(
-				new ApplyPowerAction(p, p, new TemporaryLightEcoPower(p, this.magicNumber), this.magicNumber));
+		AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p,
+				new TemporaryDarkEcoPower(p, this.magicNumber), this.magicNumber));
+		AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p,
+				new TemporaryLightEcoPower(p, this.magicNumber), this.magicNumber));
 	}
 
 	@Override
 	public void doUpgrade() {
 		this.upgradeMagicNumber(UPGRADE_DOUBLE_AMOUNT);
-    this.rawDescription = UPGRADE_DESCRIPTION;
+		this.rawDescription = UPGRADE_DESCRIPTION;
 		this.initializeDescription();
 	}
 
